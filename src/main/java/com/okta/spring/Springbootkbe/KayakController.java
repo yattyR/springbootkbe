c2fc71d17519a3f3479537db673043eda10af8f0
yattyR <yatty.destroyed@gmail.com>
2019-06-29 05:36:10 +0100

    enabled logging

--- src/main/java/com/okta/spring/Springbootkbe/KayakController.java
+++ src/main/java/com/okta/spring/Springbootkbe/KayakController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping(path = "/kayaks")
 public class KayakController {
+    private static final Logger logger = LoggerFactory.getLogger("kayak");
 
     private final KayakRepository kayakRepository;
 
@@ -19,12 +22,14 @@ public class KayakController {
     @PostMapping()
     public @ResponseBody
     Mono<Kayak> addKayak(@RequestBody Kayak kayak) {
+	    logger.info("in post");
         return kayakRepository.save(kayak);
     }
 
     @GetMapping()
     public @ResponseBody
     Flux<Kayak> getAllKayaks() {
+	    logger.info("in get");
         Flux<Kayak> result = kayakRepository.findAll();
         return result;
     }}
--- src/main/java/com/okta/spring/Springbootkbe/RootController.java
+++ src/main/java/com/okta/spring/Springbootkbe/RootController.java
@@ -3,13 +3,16 @@ package com.okta.spring.springbootkbe;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @Controller
 public class RootController {
+private static final Logger logger = LoggerFactory.getLogger("root");
 
     @GetMapping("/")
     @ResponseBody
     public Flux<String> getRoot() {
+	     logger.info("I am alive");
         return Flux.just("Alive");
     }
 }
